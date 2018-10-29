0ad88a86dc42c3bd3f06a2de7e4312f7d33798f8
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-29 21:44:32 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- 1.1 Graph/Graphs.java
+++ 1.1 Graph/Graphs.java
@@ -1,119 +1,118 @@
 /**graphimplementation.**/
 interface Graph {
-	/**.
-	 * method
-	 * @return value
-	 */
-	int vertex();
-	/**.
-	 * method
-	 * @return value
-	 */
-	int edge();
-	/**.
-	 * method
-	 * @return value
-	 * @param v value
-	 * @param w value
-	 */
-	void addEdge(int v, int w);
+    /**.
+     * method
+     * @return value
+     */
+    int vertex();
+    /**.
+     * method
+     * @return value
+     */
+    int edge();
+    /**.
+     * method
+     * @param v value
+     * @param w value
+     */
+    void addEdge(int v, int w);
 
-	// public Iterable<Integer> adj(int v);
-	//public boolean hasEdge(int v, int w);
+    // public Iterable<Integer> adj(int v);
+    //public boolean hasEdge(int v, int w);
 }
 /**graphimplementation.**/
 public class Graphs implements Graph {
-	/**
-	 * variable.
-	 */
-	private final int vertices;
-	/**
-	 * variable.
-	 */
-	private int edges;
-	/**
-	 * variable.
-	 */
-	private int size;
-	/**
-	 * variable.
-	 */
-	private Bag<Integer>[] adj;
-	/**
-	 * variable.
-	 */
-	private String[] vertex;
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 * @param Vert value.
-	 */
-	 public Graphs(final int Vert) {
-		if (Vert < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
-		this.vertices = Vert;
-		this.edges = 0;
-		adj = (Bag<Integer>[]) new Bag[Vert];
-		vertex = new String[Vert];
-		for (int v = 0; v < Vert; v++) {
-			adj[v] = new Bag<Integer>();
-		}
-		size = 0;
+    /**
+     * variable.
+     */
+    private final int vertices;
+    /**
+     * variable.
+     */
+    private int edges;
+    /**
+     * variable.
+     */
+    private int size;
+    /**
+     * variable.
+     */
+    private Bag<Integer>[] adj;
+    /**
+     * variable.
+     */
+    private String[] vertex;
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     * @param Vert value.
+     */
+     public Graphs(final int Vert) {
+        if (Vert < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
+        this.vertices = Vert;
+        this.edges = 0;
+        adj = (Bag<Integer>[]) new Bag[Vert];
+        vertex = new String[Vert];
+        for (int v = 0; v < Vert; v++) {
+            adj[v] = new Bag<Integer>();
+        }
+        size = 0;
 
-	}
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 * @return value
-	 */
-	 public int vertex() {
-		return vertices;
-	}
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 * @return value
-	 */
-	 public int edge() {
-		return edges;
-	}
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 *
-	 * @param v value
-	 * @param w value
-	 */
-	public void addEdge(final int v, final int w) {
+    }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     * @return value
+     */
+     public int vertex() {
+        return vertices;
+    }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     * @return value
+     */
+     public int edge() {
+        return edges;
+    }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     *
+     * @param v value
+     * @param w value
+     */
+    public void addEdge(final int v, final int w) {
 
-		// validateVertex(v);
-		// validateVertex(w);
-	if(v == w) {
-		System.out.println(vertices + " vertices, " + edges + " edges");
-		System.out.println("No edges");
-		return;
-	}
-		edges++;
-		adj[v].add(w);
-	   adj[w].add(v);
-	}
+        // validateVertex(v);
+        // validateVertex(w);
+    if (v == w) {
+        System.out.println(vertices + " vertices, " + edges + " edges");
+        System.out.println("No edges");
+        return;
+    }
+        edges++;
+        adj[v].add(w);
+       adj[w].add(v);
+    }
 
-	public void addvertices(final String s) {
-		vertex[size++] = s;
+    public void addvertices(final String s) {
+        vertex[size++] = s;
 
-	}
-	 public String toString() {
-		StringBuilder s = new StringBuilder();
-		s.append(vertices + " vertices, " + edges + " edges" + "\n");
-		for (int v = 0; v < vertices; v++) {
-			s.append(vertex[v] + ": ");
-			for (int w : adj[v]) {
-				s.append(vertex[w] + " ");
-				//s.substring(0,s.length() - 1);
-			}
-			s.append("\n");
-		}
-		return s.toString();
-	}
+    }
+     public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(vertices + " vertices, " + edges + " edges" + "\n");
+        for (int v = 0; v < vertices; v++) {
+            s.append(vertex[v] + ": ");
+            for (int w : adj[v]) {
+                s.append(vertex[w] + " ");
+                //s.substring(0,s.length() - 1);
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
 
 }
 
