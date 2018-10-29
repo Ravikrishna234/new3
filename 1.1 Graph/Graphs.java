b064946e3880c10a9995a18b2f407487c899c4d2
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-29 21:31:04 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- 1.1 Graph/Graphs.java
+++ 1.1 Graph/Graphs.java
@@ -4,114 +4,116 @@ interface Graph {
 	 * method
 	 * @return value
 	 */
-    public int V();
-    /**.
+	int vertex();
+	/**.
 	 * method
 	 * @return value
 	 */
-    public int E();
-    /**.
+	int edge();
+	/**.
 	 * method
 	 * @return value
+	 * @param v value
+	 * @param w value
 	 */
-    public void addEdge(int v, int w);
+	void addEdge(int v, int w);
 
-    // public Iterable<Integer> adj(int v);
-    //public boolean hasEdge(int v, int w);
+	// public Iterable<Integer> adj(int v);
+	//public boolean hasEdge(int v, int w);
 }
 /**graphimplementation.**/
 public class Graphs implements Graph {
 	/**
 	 * variable.
 	 */
-    private final int V;
-    /**
+	private final int V;
+	/**
 	 * variable.
 	 */
-    private int E;
-    /**
+	private int E;
+	/**
 	 * variable.
 	 */
-    private int size;
-    /**
+	private int size;
+	/**
 	 * variable.
 	 */
-    private Bag<Integer>[] adj;
-    /**
+	private Bag<Integer>[] adj;
+	/**
 	 * variable.
 	 */
-    private String[] vertex;
-    /**
-     * @brief [brief description]
-     * @details [long description]
-     * @param V value.
-     */
-     public Graphs(int V) {
-        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
-        this.V = V;
-        this.E = 0;
-        adj = (Bag<Integer>[]) new Bag[V];
-        vertex = new String[V];
-        for (int v = 0; v < V; v++) {
-            adj[v] = new Bag<Integer>();
-        }
-        size = 0;
+	private String[] vertex;
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 * @param V value.
+	 */
+	 public Graphs(int V) {
+		if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
+		this.V = V;
+		this.E = 0;
+		adj = (Bag<Integer>[]) new Bag[V];
+		vertex = new String[V];
+		for (int v = 0; v < V; v++) {
+			adj[v] = new Bag<Integer>();
+		}
+		size = 0;
 
-    }
-    /**
-     * @brief [brief description]
-     * @details [long description]
-     * @return value
-     */
-     public int V() {
-        return V;
-    }
-    /**
-     * @brief [brief description]
-     * @details [long description]
-     * @return value
-     */
-     public int E() {
-        return E;
-    }
-    /**
-     * @brief [brief description]
-     * @details [long description]
-     *
-     * @param v value
-     * @param w value
-     */
-    public void addEdge(int v, int w) {
+	}
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 * @return value
+	 */
+	 public int vertex() {
+		return V;
+	}
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 * @return value
+	 */
+	 public int edge() {
+		return E;
+	}
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 *
+	 * @param v value
+	 * @param w value
+	 */
+	public void addEdge(int v, int w) {
 
-        // validateVertex(v);
-        // validateVertex(w);
-    if(v == w) {
-    	System.out.println(V + " vertices, " + E + " edges");
-    	System.out.println("No edges");
-    	return;
-    }
-        E++;
-        adj[v].add(w);
-       adj[w].add(v);
-    }
+		// validateVertex(v);
+		// validateVertex(w);
+	if(v == w) {
+		System.out.println(V + " vertices, " + E + " edges");
+		System.out.println("No edges");
+		return;
+	}
+		E++;
+		adj[v].add(w);
+	   adj[w].add(v);
+	}
 
-  	public void addvertices(String s) {
-  		vertex[size++] = s;
+	public void addvertices(String s) {
+		vertex[size++] = s;
 
-  	}
-  	 public String toString() {
-        StringBuilder s = new StringBuilder();
-        s.append(V + " vertices, " + E + " edges" + "\n");
-        for (int v = 0; v < V; v++) {
-            s.append(vertex[v] + ": ");
-            for (int w : adj[v]) {
-                s.append(vertex[w] + " ");
-                //s.substring(0,s.length() - 1);
-            }
-            s.append("\n");
-        }
-        return s.toString();
-    }
+	}
+	 public String toString() {
+		StringBuilder s = new StringBuilder();
+		s.append(V + " vertices, " + E + " edges" + "\n");
+		for (int v = 0; v < V; v++) {
+			s.append(vertex[v] + ": ");
+			for (int w : adj[v]) {
+				s.append(vertex[w] + " ");
+				//s.substring(0,s.length() - 1);
+			}
+			s.append("\n");
+		}
+		return s.toString();
+	}
 
 }
 
