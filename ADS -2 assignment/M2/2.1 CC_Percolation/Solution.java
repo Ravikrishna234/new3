e52936cab1d1580e11e623130e393a9da910c5b3
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-30 17:49:29 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS -2 assignment/M2/2.1 CC_Percolation/Solution.java
+++ ADS -2 assignment/M2/2.1 CC_Percolation/Solution.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
 /**PERCOLATION.**/
-class Solution {
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 */
-	private Solution() { }
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 * @param args value
-	 */
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);
-		int n = Integer.parseInt(s.nextLine());
-		//
-		Percolation p = new Percolation(n);
-		while (s.hasNextLine()) {
-		String[] tokens = s.nextLine().split(" ");
-		p.open(Integer.parseInt(tokens[0]) - 1, Integer.parseInt(tokens[1]) - 1);
-	}
-	//Percolation p = new Percolation(n);
-	System.out.println(p.percolates());
-
-	}
+final class Solution {
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     */
+    private Solution() { }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     * @param args value
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = Integer.parseInt(s.nextLine());
+        //
+        Percolation p = new Percolation(n);
+        while (s.hasNextLine()) {
+        String[] tokens = s.nextLine().split(" ");
+        p.open(Integer.parseInt(tokens[0]) - 1,
+            Integer.parseInt(tokens[1]) - 1);
+    }
+    //Percolation p = new Percolation(n);
+    System.out.println(p.percolates());
+    }
 }
+
