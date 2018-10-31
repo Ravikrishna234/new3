e7b14f5f8eccd051d904f65aa4ab5eb2d0fbc4b2
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-31 18:40:24 +0530

    "ads2_m3_a2_1: 4/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS -2 assignment/M3/3.2 Bipartite/Cycle.java
+++ ADS -2 assignment/M3/3.2 Bipartite/Cycle.java
@@ -83,30 +83,4 @@ class DirectedCycle {
     public Iterable<Integer> cycle() {
         return cycle;
     }
-
-
-    /**
-     * @return value
-     * Time complexity is O(N)
-     */
-    private boolean check() {
-
-        if (isbipartite()) {
-            // verify cycle
-            int first = -1, last = -1;
-            for (int v : cycle()) {
-                if (first == -1) {
-                    first = v;
-                }
-                last = v;
-            }
-    if (first != last) {
-        System.err.printf(
-            "cycle begins with %d and ends with %d\n", first, last);
-                return false;
-            }
-        }
-        return true;
-    }
 }
-
