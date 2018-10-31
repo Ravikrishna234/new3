b8d77ed663745fcb382104f1c35a583ef3da18c8
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-31 14:53:28 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS -2 assignment/M3/3.1 Directed Cycle/Solution.java
+++ ADS -2 assignment/M3/3.1 Directed Cycle/Solution.java
@@ -1,17 +1,28 @@
-import java.util.*;
-class Solution {
-	public static void main(String[] args) {
+import java.util.Scanner;
+/**HASACYCLE.**/
+final class Solution {
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 */
+	private Solution() { }
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 * @param args value
+	 */
+	public static void main(final String[] args) {
 		Scanner s = new Scanner(System.in);
 		int num1 = Integer.parseInt(s.nextLine());
 		int num2 = Integer.parseInt(s.nextLine());
 		DiGraph dg = new DiGraph(num1);
 
-		while(s.hasNextLine()) {
+		while (s.hasNextLine()) {
 			String[] tokens = s.nextLine().split(" ");
 			dg.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
 		}
 			DirectedCycle cycle = new DirectedCycle(dg);
-		if(cycle.hasCycle()){
+		if (cycle.hasCycle()){
 			System.out.println("Cycle exists.");
 
 		} else {
