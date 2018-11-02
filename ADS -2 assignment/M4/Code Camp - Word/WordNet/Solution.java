a00a9d0bab5b53b7adf8a1e16882fd51408a29e6
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-11-02 18:32:08 +0530

    "ads2_m4_a1_1: 5/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS -2 assignment/M4/Code Camp - Word/WordNet/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String sysnsets = "Files" + "\\" + s.nextLine();
+		String hypernymsets = "Files" + "\\" + s.nextLine();
+		String input = s.nextLine();
+		try {
+		WordNet w = new WordNet(sysnsets, hypernymsets);
+		if(input.equals("Graph"))
+		if(!w.isflag()) {
+			w.print();
+		} else {
+			while(s.hasNextLine()) {
+				String[] tokens = s.nextLine().split(" ");
+				String str = w.sap(tokens[0], tokens[1]);
+				int dis = w.distance(tokens[0], tokens[1]);
+				System.out.println("distance = " + dis + ", ancestor = " +str);
+			}
+		}
+		} catch(Exception e) {
+			System.out.println("IllegalArgumentException");
+
+		}
+	}
+}
