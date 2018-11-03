aafe04dba62611b36d0eafc69b9b30ff54d3d5c9
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-11-03 12:14:49 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS -2 assignment/m6/ADS-2Week-1/Solution.java
@@ -0,0 +1,140 @@
+import java.util.Scanner;
+import java.util.Arrays;
+/**PAGERANK.**/
+class PageRank {
+	/**
+	 * variable.
+	 */
+	private Digraph graph;
+	/**
+	 * variable.
+	 */
+	private float[] a;
+	/**
+	 * variable.
+	 */
+	private int vertices;
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 * @param g value
+	 */
+	PageRank(Digraph g) {
+		this.graph = g;
+		this.vertices = g.V();
+		a = new float[this.vertices];
+		float f = (float)vertices;
+		for(int i = 0; i < vertices; i++) {
+			a[i] = 1/f;
+			//System.out.println(a[i]);
+		}
+		int temp = vertices;
+		while(temp > 0) {
+		for(int i = 0; i < vertices; i++) {
+			Queue q = g.connected(i);
+			int t = q.size();
+			if(t == 0) {
+				a[i] = 0;
+			}
+
+
+			while(t > 0) {
+				 int c = (int)q.dequeue();
+				a[i] = a[c] / g.outdegree(c);
+				t--;
+			}
+			}
+		temp--;
+		}
+
+	}
+	//public void computerank(Digraph g) {
+	// 	for(int i = 0; i < vertices; i++) {
+	// 		a[i] = 1/vertices;
+	// 		//System.out.println(a[i]);
+	// 	}
+	// 	int temp = vertices;
+	// 	while(temp > 0) {
+	// 	for(int i = 0; i < vertices; i++) {
+	// 		Queue q = g.connected(i);
+	// 		int t = q.size();
+	// 		if(t == 0) {
+	// 			a[i] = 0;
+	// 		}
+	// 		while(t > 0) {
+	// 			 int c = (int)q.dequeue();
+	// 			a[i] = a[c] / g.outdegree(c);
+	// 			t--;
+	// 		}
+	// 		}
+	// 	}
+	// 	temp--;
+	// }
+	public String toString() {
+		String str = "";
+		for(int i = 0; i < vertices; i++) {
+			str += i + "-" + a[i] + "\n";
+		}
+		return str;
+
+	}
+}
+/**pagerank.**/
+class WebSearch {
+
+}
+
+/**pagerank.**/
+public class Solution {
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 */
+	private Solution() { }
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 * @param args value
+	 */
+	public static void main(final String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = Integer.parseInt(s.nextLine());
+		int j = 0;
+		Digraph g = new Digraph(n);
+		while (j < n) {
+			String[] tokens = s.nextLine().split(" ");
+			for (int i = 1; i < tokens.length; i++) {
+				g.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[i]));
+			}
+			j++;
+		}
+		System.out.println(g);
+		// read the first line of the input to get the number of vertices
+
+		// iterate count of vertices times
+		// to read the adjacency list from std input
+		// and build the graph
+
+
+		// Create page rank object and pass the graph object to the constructor
+		PageRank p = new PageRank(g);
+		//p.computerank(g);
+		System.out.println(p);
+		// print the page rank object
+
+		// This part is only for the final test case
+
+		// File path to the web content
+		String file = "WebContent.txt";
+		//WebSearch w = new WebSearch(file);
+
+		// instantiate web search object
+		// and pass the page rank object and the file path to the constructor
+
+		// read the search queries from std in
+		// remove the q= prefix and extract the search word
+		// pass the word to iAmFeelingLucky method of web search
+		// print the return value of iAmFeelingLucky
+
+	}
+}
