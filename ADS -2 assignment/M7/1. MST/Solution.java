d50d392ebc1b88f1a2926ae7d1dcbfd74730bb73
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-11-05 19:39:41 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS -2 assignment/M7/1. MST/Solution.java
+++ ADS -2 assignment/M7/1. MST/Solution.java
@@ -1,33 +1,33 @@
 import java.util.Scanner;
-import java.util.Arrays;
 /**MINIMUMDISTANCE.**/
-class Solution {
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 */
-	private Solution() { }
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 * Time complexity is O(N)
-	 * @param args [description]
-	 */
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);
-		int vert = Integer.parseInt(s.nextLine());
-		int testcases = Integer.parseInt(s.nextLine());
-		EdgeWeightedGraph wtEdge = new EdgeWeightedGraph(vert);
-		for (int i = 0; i < testcases; i++) {
-			String[] inp = s.nextLine().split(" ");
-			Edge e = new Edge(Integer.parseInt(inp[0]),
-				Integer.parseInt(inp[1]),
-				Double.parseDouble(inp[2]));
-			wtEdge.addEdge(e);
-		}
-		LazyPrimMST p =  new LazyPrimMST(wtEdge);
-		// System.out.println(p.edges());
-		System.out.format("%.5f",p.weight());
-	}
+final class Solution {
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     */
+    private Solution() { }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     * Time complexity is O(N)
+     * @param args [description]
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int vert = Integer.parseInt(s.nextLine());
+        int testcases = Integer.parseInt(s.nextLine());
+        EdgeWeightedGraph wtEdge = new EdgeWeightedGraph(vert);
+        for (int i = 0; i < testcases; i++) {
+            String[] inp = s.nextLine().split(" ");
+            Edge e = new Edge(Integer.parseInt(inp[0]),
+                Integer.parseInt(inp[1]),
+                Double.parseDouble(inp[2]));
+            wtEdge.addEdge(e);
+        }
+        LazyPrimMST p =  new LazyPrimMST(wtEdge);
+        // System.out.println(p.edges());
+        System.out.format("%.5f", p.weight());
+    }
 }
+
 
