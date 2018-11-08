37ccef6cb25e2a1839748ebaa573f9d6eb2f2ad2
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-11-08 14:48:06 +0530

    "ads2_m6_a1_1: 4/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS -2 assignment/m6/ADS-2Week-1/Solution.java
+++ ADS -2 assignment/m6/ADS-2Week-1/Solution.java
@@ -14,18 +14,18 @@ class PageRank {
      * variable.
      */
     private int vertices;
-     /**
-     * variable.
-     */
-     private double[] b;
-      /**
-     * variable.
-     */
-      private final int thousand = 1000;
-       /**
-     * variable.
-     */
-       private double result;
+    /**
+    * variable.
+    */
+    private double[] b;
+    /**
+    * variable.
+    */
+    private final int thousand = 1000;
+    /**
+    * variable.
+    */
+    private double result;
 
     /**
      * @brief [brief description]
@@ -45,27 +45,27 @@ class PageRank {
      * @param g value
      */
     public void computerank(final Digraph g) {
-     double divide = (double) vertices;
+        double divide = (double) vertices;
         for (int i = 0; i < vertices; i++) {
             a[i] = 1 / divide;
             //System.out.println(a[i]);
         }
         int temp = vertices;
         Digraph reverse = g.reverse();
-       for (int i = 0; i < thousand; i++) {
-        for (int j = 0; j < temp; j++) {
-            result = 0.0;
-            for (int k : reverse.adj(j)) {
-                result += ((a[k])) / ((double) ((g.outdegree(k))));
+        for (int i = 0; i < thousand; i++) {
+            for (int j = 0; j < temp; j++) {
+                result = 0.0;
+                for (int k : reverse.adj(j)) {
+                    result += ((a[k])) / ((double) ((g.outdegree(k))));
+                }
+                b[j] = result;
             }
-            b[j] = result;
+            if (Arrays.equals(a, b)) {
+                break;
+            } else {
+                a = b.clone();
+            }
         }
-        if(Arrays.equals(a,b)) {
-            break;
-        } else {
-            a = b.clone();
-        }
-       }
     }
     /**
      * @brief [brief description]
@@ -107,22 +107,23 @@ final class Solution {
         Digraph g1 = new Digraph(n);
         while (j < n) {
             String[] tokens = s.nextLine().split(" ");
-            if(tokens.length >= 2) {
-            for (int i = 1; i < tokens.length; i++) {
-                g.addEdge(Integer.parseInt(tokens[0]),
-                    Integer.parseInt(tokens[i]));
-                g1.addEdge(Integer.parseInt(tokens[0]),
-                    Integer.parseInt(tokens[i]));
-            }
-            j++;
-        }  else {
+            if (tokens.length >= 2) {
+                for (int i = 1; i < tokens.length; i++) {
+                    g.addEdge(Integer.parseInt(tokens[0]),
+                        Integer.parseInt(tokens[i]));
+                    g1.addEdge(Integer.parseInt(tokens[0]),
+                               Integer.parseInt(tokens[i]));
+                }
+            }  else {
                 for (int k = 0; k < n; k++) {
                     if (k != Integer.parseInt(tokens[0])) {
                         g1.addEdge(Integer.parseInt(tokens[0]), k);
                     }
+
                 }
             }
-    }
+            j++;
+        }
         System.out.println(g);
         // for (int i = 0; i < g.V(); i++) {
         //     if (g.outdegree(i) == 0) {
