ca88f423bcfa8cc2fefc45c88e25d00b1d992eb8
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-11-12 15:25:37 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS -2 assignment/M10/Seam Carving/ResizeDemo.java
+++ ADS -2 assignment/M10/Seam Carving/ResizeDemo.java
@@ -13,39 +13,40 @@ public final class ResizeDemo {
      *
      * @param      args  The arguments
      */
-    public static void main(final String[] args) {
-        if (args.length != 2 + 1) {
-            StdOut.println(
-                "Usage:\njava ResizeDemo [image filename] [num cols to remove] [num rows to remove]");
-            return;
-        }
+    // public static void main(final String[] args) {
+    //     if (args.length != 2 + 1) {
+    //         StdOut.println(
+    //             "Usage:\njava ResizeDemo [image filename]
+    //             [num cols to remove] [num rows to remove]");
+    //         return;
+    //     }
 
-        Picture inputImg = new Picture(args[0]);
-        int removeColumns = Integer.parseInt(args[1]);
-        int removeRows = Integer.parseInt(args[2]);
+    //     Picture inputImg = new Picture(args[0]);
+    //     int removeColumns = Integer.parseInt(args[1]);
+    //     int removeRows = Integer.parseInt(args[2]);
 
-        StdOut.printf("image is %d columns by %d rows\n",
-            inputImg.width(), inputImg.height());
-        SeamCarver sc = new SeamCarver(inputImg);
+    //     StdOut.printf("image is %d columns by %d rows\n",
+    //         inputImg.width(), inputImg.height());
+    //     SeamCarver sc = new SeamCarver(inputImg);
 
-        Stopwatch sw = new Stopwatch();
+    //     Stopwatch sw = new Stopwatch();
 
-        for (int i = 0; i < removeRows; i++) {
-            int[] horizontalSeam = sc.findHorizontalSeam();
-            sc.removeHorizontalSeam(horizontalSeam);
-        }
+    //     for (int i = 0; i < removeRows; i++) {
+    //         int[] horizontalSeam = sc.findHorizontalSeam();
+    //         sc.removeHorizontalSeam(horizontalSeam);
+    //     }
 
-        for (int i = 0; i < removeColumns; i++) {
-            int[] verticalSeam = sc.findVerticalSeam();
-            sc.removeVerticalSeam(verticalSeam);
-        }
-        Picture outputImg = sc.picture();
+    //     for (int i = 0; i < removeColumns; i++) {
+    //         int[] verticalSeam = sc.findVerticalSeam();
+    //         sc.removeVerticalSeam(verticalSeam);
+    //     }
+    //     Picture outputImg = sc.picture();
 
-        StdOut.printf("new image size is %d columns by %d rows\n",
-            sc.width(), sc.height());
+    //     StdOut.printf("new image size is %d columns by %d rows\n",
+    //         sc.width(), sc.height());
 
-        StdOut.println("Resizing time: " + sw.elapsedTime() + " seconds.");
-        inputImg.show();
-        outputImg.show();
-    }
+    //     StdOut.println("Resizing time: " + sw.elapsedTime() + " seconds.");
+    //     inputImg.show();
+    //     outputImg.show();
+    // }
 }
