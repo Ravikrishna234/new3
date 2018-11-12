3283483641bb74a7c824c6ef6bff2625b5ee901c
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-11-12 14:56:52 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS -2 assignment/M13/Radix Sorts/Solution.java
+++ ADS -2 assignment/M13/Radix Sorts/Solution.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 /**MOSTSIGNIFICANTDIGITSORT.**/
 final class Solution {
-	private Solution() {
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     */
+    private Solution() {
 
-	}
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 *
-	 * @param args value
-	 */
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);
-		int n = Integer.parseInt(s.nextLine());
-		String[] tokens = new String[n];
-		for (int i = 0; i < n; i++) {
-			tokens[i] = s.nextLine();
-		}
-		MSD m = new MSD();
-		m.sort(tokens);
-	System.out.println(m.toString(tokens));
-	}
+    }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     *
+     * @param args value
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = Integer.parseInt(s.nextLine());
+        String[] tokens = new String[n];
+        for (int i = 0; i < n; i++) {
+            tokens[i] = s.nextLine();
+        }
+        MSD m = new MSD();
+        m.sort(tokens);
+    System.out.println(m.toString(tokens));
+    }
 }
 
