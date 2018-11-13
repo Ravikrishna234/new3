5af2477518951f84f35763c5aa1bcdc02a65dfde
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-11-13 16:48:16 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS -2 assignment/MM14/Assignment-1/Solution.java
+++ ADS -2 assignment/MM14/Assignment-1/Solution.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
-import java.util.Arrays;
-import java.util.ArrayList;
+/**STRINGSUFFIX.**/
 public class Solution {
-	public static void main(String[] args) {
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 */
+	private Solution() { }
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 * Time complexity is no of words * word length
+	 * @param args [description]
+	 */
+	public static void main(final String[] args) {
 		Scanner s = new Scanner(System.in);
 		String[] words = loadWords();
 
 		TST<Integer> ts = new TST<Integer>();
-		for(String str : words) {
+		for (String str : words) {
 		SuffixArray sf = new SuffixArray(str);
 		for (int i = 0; i < str.length(); i++) {
 
